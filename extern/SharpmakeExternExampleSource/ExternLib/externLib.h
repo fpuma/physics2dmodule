d40dba89d10b4bc01b01fdf6cb9b8ecbe1e5d9ca
fpuma <fpuma88@gmail.com>
2020-11-15 22:50:11 +0100

    Removing sharpmake template example files

--- extern/SharpmakeExternExampleSource/ExternLib/externLib.h
+++
@@ -1,9 +0,0 @@
-
-namespace Examples::Extern::StaticLibrary
-{
-    class ExternClass
-    {
-    public:
-        void someMethod();
-    };
-}
--- source/SharpmakeExamplesSource/MyLib/myLib.h
+++
@@ -1,9 +0,0 @@
-
-namespace Examples::MyProjects::StaticLibrary
-{
-    class MyClass
-    {
-    public:
-        void someMethod();
-    };
-}
